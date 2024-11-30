177570f9bd2c78b449273ad9fd6f164b9b2048cf
Illia <ilaruslanovich7@gmail.com>
2024-11-30 16:44:04 +0100

    created base serializers

---
+++ library_borrow/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from library_borrow.models import *
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = "__all__"
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
+
